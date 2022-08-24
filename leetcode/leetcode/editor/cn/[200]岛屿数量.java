7d16fc86211015c8a8ed1c2dcefbb0583d4ff058
konghao <kongh@chinatelecom.cm>
2022-08-24 21:57:03 +0800

    200、岛屿数量（dfs深度遍历）

---
+++ leetcode/leetcode/editor/cn/[200]岛屿数量.java
@@ -0,0 +1,82 @@
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。 
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。 
+//
+// 此外，你可以假设该网格的四条边均被水包围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 300 
+// grid[i][j] 的值为 '0' 或 '1' 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 
+// 👍 1856 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIslands(char[][] grid) {
+        //统计岛屿的数量
+        int count = 0;
+
+        /**
+         * 当grid[i][j]为陆地的时候进行上下左右四个方向的搜索
+         */
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    //深度遍历寻找陆地，遍历完成后为一个岛屿
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        //上下左右边界之外，且不为陆地为海水的地方直接返回
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        //将遍历的陆地grid[i][j]进入以它为中心的上下左右陆地找寻时，将此位置变为海水，保证不会重复统计
+        grid[i][j] = '0';
+        //四个方向的深度遍历
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
